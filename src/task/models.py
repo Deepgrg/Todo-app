985978f13cd387a0bdb90fc62d65a8bd36af3638
Dip Gurung <deepgrg03@gmail.com>
2020-03-04 21:52:26 +0545

    Models for the tasks created and registered.

--- src/task/models.py
+++ src/task/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    topic = models.CharField(max_length = 180)
+    description = models.TextField(blank= True , null = True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.topic
