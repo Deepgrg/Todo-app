329ed059f08bbef125ce9209a1b5333d792854f5
Dip Gurung <deepgrg03@gmail.com>
2020-03-04 23:40:54 +0545

    CRUD view of the todo app completed.

--- src/task/urls.py
+++ src/task/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('' ,views.indexPage , name= "index_page" ),
+    path('addTask/' , views.addTask , name="addTask_page"),
+    path('updateTask/<str:pk>/' , views.updateTask , name="updateTask_page"),
+    path('deleteTask/<str:pk>/' , views.deleteTask , name="deleteTask_page"),
 ] 
--- src/task/views.py
+++ src/task/views.py
@@ -1,5 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import *
+from .forms import TaskForm
 
 def indexPage(request):
-    context= {}
+    tasks = Task.objects.all();
+    context= {
+        'tasks': tasks,
+    }
     return render(request , "task/indexPage.html", context)
+
+
+def addTask(request):
+    task_form = TaskForm()
+    if request.method =="POST" :
+        task_form=TaskForm(request.POST)
+        if task_form.is_valid():
+            task_form.save()
+            return redirect('index_page')
+    context={
+        'form': task_form,
+    }
+    return render(request, "task/addTask.html" , context)
+
+def updateTask(request , pk):
+    task_detail = Task.objects.get(id = pk)
+    task_form= TaskForm(instance= task_detail)
+
+    if request.method== "POST":
+        task_form = TaskForm(request.POST , instance=task_detail)
+        if task_form.is_valid():
+            task_form.save()
+            return redirect('index_page')
+
+    context={
+        'form':task_form,
+    }
+    return render(request,"task/updateTask.html", context)
+
+def deleteTask(request , pk):
+    task_detail = Task.objects.get(id=pk)
+    if request.method =="POST":
+        task_detail.delete()
+        return redirect('index_page')
+    context ={
+        'task': task_detail,
+    }
+    return render(request , 'task/deleteTask.html' , context)
