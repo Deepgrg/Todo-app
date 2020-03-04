5d94c2ca8fca561ffcb78071955194dda4f18bbe
Dip Gurung <deepgrg03@gmail.com>
2020-03-05 02:11:03 +0545

    User register and login page created succesfully -> authenticating page for user left

--- src/account/urls.py
+++ src/account/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns =[
     path('' , views.accountList , name='accountList_page'),
+    path('register/' , views.register , name='register_page'),
+    path('login/' , views.loginPage , name='login_page'),
+    path('user/' , views.userAccount , name = 'userAccount_page'),
+    
 ]
--- src/account/views.py
+++ src/account/views.py
@@ -1,10 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
+from django.contrib.auth import authenticate,login,logout
+
+from .forms import CreateUser
 
 def accountList(request):
+    user_count=User.objects.count()
     context= {
-
+        'user_count': user_count,
     }
     return render(request , 'account/accountList.html', context)
 
 
+def register(request):
+    form = CreateUser()
+    if request.method == "POST":
+        form = CreateUser(request.POST)
+        if form.is_valid():
+            form.save()
+            user= form.cleaned_data.get('username')
+            messages.success(request, 'Account was succesfully created for ' + user)
+            return redirect('login_page')
+
+    context={
+        'form': form,
+    }
+    return render(request , 'account/register.html' ,context)
+
+
+def loginPage(request):
+    if request.method =="POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username = username , password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('userAccount_page')
+        else:
+            messages.info(request,'Username or password is incorrect')
+
+    context={
+
+    }
+    return render(request, 'account/login.html', context)
+
+
+def userAccount(request):
+    context ={
+
+    }
+    return render(request, 'account/userAccount.html' , context)
